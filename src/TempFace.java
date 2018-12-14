eb272ce3a735df7d77b092d61a334b8a107e2a5e
Palmyre <palmyra.wuertz@polytechnique.edu>
2018-12-14 19:04:15 +0100

    Add class TempFace

---
+++ src/TempFace.java
@@ -0,0 +1,18 @@
+import Jcg.geometry.Point_3;
+import Jcg.polyhedron.Face;
+
+public class TempFace extends Face<Point_3> implements Cloneable {
+	
+	Face<Point_3> copiedFace;
+	
+	public TempFace(Face<Point_3> f) {
+		super();
+		this.tag = f.tag;
+		this.copiedFace = f;
+	}
+	
+	@Override
+	public TempFace clone() throws CloneNotSupportedException {
+		return (TempFace) super.clone();
+	}
+}
