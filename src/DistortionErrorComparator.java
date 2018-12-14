6161d6cba6ead437ceb419878c3b3633ae6819de
Palmyre <palmyra.wuertz@polytechnique.edu>
2018-12-14 19:04:03 +0100

    Add class DistortionErrorComparator

---
+++ src/DistortionErrorComparator.java
@@ -0,0 +1,23 @@
+import java.util.Comparator;
+
+import Jcg.geometry.*;
+import Jcg.polyhedron.*;
+
+public class DistortionErrorComparator implements Comparator<Face<Point_3>> {
+
+	ShapeApproximation sa;
+	public DistortionErrorComparator(ShapeApproximation sa) {
+		this.sa = sa;
+	}
+    @Override
+    public int compare(Face<Point_3> f, Face<Point_3> g)
+    {
+    	double distanceF = sa.barycenter_proxy_metric(f, sa.proxies[f.tag]);
+    	double distanceG = sa.barycenter_proxy_metric(g, sa.proxies[g.tag]);
+        if (distanceF < distanceG)
+            return -1;
+        if (distanceF > distanceG)
+            return 1;
+        return 0;
+    }
+}
