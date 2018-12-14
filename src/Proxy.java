0e8188a2b80dfff6193a67d7c7524f90ff02f732
Palmyre <palmyra.wuertz@polytechnique.edu>
2018-12-14 19:05:42 +0100

    Add normal field to Proxy

--- src/Proxy.java
+++ src/Proxy.java
@@ -1,10 +1,12 @@
 import Jcg.geometry.Point_3;
+import Jcg.geometry.Vector_3;
 
 public class Proxy {
-	// normal
-	Point_3 X;
+	Point_3 X; // Center
+	Vector_3 N; // Normal
 	
-	public Proxy(Point_3 X) {
+	public Proxy(Point_3 X, Vector_3 N) {
 		this.X = X;
+		this.N= N;
 	}
 }
