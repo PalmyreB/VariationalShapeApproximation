ff3de241b5d7ea9a333f589d04d02d3d41df449d
Palmyre <palmyra.wuertz@polytechnique.edu>
2018-12-14 22:55:10 +0100

    Proxy fitting

--- src/ShapeApproximation.java
+++ src/ShapeApproximation.java
@@ -13,31 +13,22 @@ public class ShapeApproximation {
 	public Polyhedron_3<Point_3> polyhedron3D;
 	public ArrayList<List<Face<Point_3>>> partition;
 	public Proxy[] proxies;
+	public int k;
 	
-	public ShapeApproximation(Polyhedron_3<Point_3> polyhedron3D) {
+	public ShapeApproximation(Polyhedron_3<Point_3> polyhedron3D, int k) {
 		this.polyhedron3D=polyhedron3D;
+		this.k = k;
 	}
 
 	/**
 	 * The main method performing the approximation process
 	 * To be implemented
 	 */
-	public void approximate(int k) {
-		// First k-partitioning
-		// k vertices are randomly selected
-		this.k_partitioning(k);
-		
-		// Geometry partitioning
-		// Knowing a current set of proxies P , we wish to update the partition
-		// R while trying to minimize the distortion error E(R, P ) in the
-		// process.
-		this.geometry_partitioning(k);
-		
-		// Proxy fitting
-		// this.proxy_fitting();
-		
-		
-		// Meshing
+	public void approximate() {
+		this.k_partitioning();
+		this.geometry_partitioning();
+		this.proxy_fitting();
+		// TODO: Meshing
 	}
 	
 	/**
@@ -46,7 +37,7 @@ public class ShapeApproximation {
 	 * each of these triangles are assigned a proxy defined as the
 	 * triangle’s barycenter and its normal.
 	 */
-	public void k_partitioning(int k) {
+	public void k_partitioning() {
 		for(Face<Point_3> face : this.polyhedron3D.facets)
 			face.tag  = -1;
 
@@ -93,7 +84,12 @@ public class ShapeApproximation {
 	    
 	}
 	
-	private void geometry_partitioning(int k) {
+	/**
+	 * Geometry partitioning
+	 * Knowing a current set of proxies P , we wish to update the partition
+	 * R while trying to minimize the distortion error E(R, P ) in the process.
+	 */
+	private void geometry_partitioning() {
 		
 		// Initial seeding
 		TempFace nearestFace;
@@ -165,12 +161,23 @@ public class ShapeApproximation {
 	}
 	
 	private void proxy_fitting() {
-		throw new RuntimeException("Not implemented yet");
+		for(int i = 0; i < k; i++) {
+			List<Face<Point_3>> region = this.partition.get(i);
+			int n = region.size();
+			Vector_3 N = new Vector_3(0,0,0);
+			Point_3[] barycenters = new Point_3[n];
+			for(int j = 0; j < n; j++) {
+				Face<Point_3> f = region.get(j);
+				barycenters[j] = this.getBarycenterOfFace(f);
+				N = N.sum(this.getNormalOfFace(f).multiplyByScalar(this.areaOfTriangle(f)));
+			}
+			this.proxies[i].X.barycenter(barycenters);
+			this.proxies[i].N = N.divisionByScalar(Math.sqrt((double) N.squaredLength()));
+		}
 	}
 	
 	/**
 	 * Distance between the barycenters of two surfaces.
-	 * Used in first approximation.
 	 */
 	private double barycenter_metric(Face<Point_3> surface1, Face<Point_3> surface2) {
 		Point_3 barycenter1 = this.getBarycenterOfFace(surface1);
@@ -183,12 +190,38 @@ public class ShapeApproximation {
 		return (double)barycenter1.distanceFrom(proxy.X);
 	}
 	
+	/**
+	 * Area calculated thanks to Heron's formula
+	 * @param triangle
+	 * @return
+	 */
+	public double areaOfTriangle(Face<Point_3> triangle) {
+		double S; // semiperimeter
+		double A; // area
+		double a, b, c; // side lengths of triangle
+
+		if(triangle instanceof TempFace)
+			triangle = ((TempFace)triangle).copiedFace;
+
+		int[] vertexIndices = triangle.getVertexIndices(this.polyhedron3D);
+		Point_3[] points = new Point_3[3];
+		for(int i=0; i < 3; i++)
+			points[i] = this.polyhedron3D.vertices.get(vertexIndices[i]).getPoint();
+		a = (double) points[0].distanceFrom(points[1]);
+		b = (double) points[1].distanceFrom(points[2]);
+		c = (double) points[0].distanceFrom(points[2]);
+		S = 0.5 * (a+b+c);
+		A = Math.sqrt(S*(S-a)*(S-b)*(S-c));
+		
+		return A;
+	}
+	
 	public double L2_metric(Face<Point_3> surface, Proxy proxy) {
 		throw new RuntimeException("Not implemented yet");
 	}
 	
 	public double L21_metric(Face<Point_3> surface, Proxy proxy) {
-		// DIstance between the normals
+		// Distance between the normals
 		Point_3 x = this.getBarycenterOfFace(surface);
 		Vector_3 n = this.getNormalOfFace(surface);
 		Vector_3 prod = n.crossProduct(proxy.N);
@@ -201,24 +234,8 @@ public class ShapeApproximation {
 		prod = prod.divisionByScalar(norm);
 		Vector_3 diff = (Vector_3) x.minus(proxy.X);
 		double distance = Math.abs((double)prod.innerProduct(diff));
-
-		// Area calculated thanks to Heron's formula
-		double S; // semiperimeter
-		double A; // area
-		double a, b, c; // side lengths of triangle
-
-		if(surface instanceof TempFace)
-			surface = ((TempFace)surface).copiedFace;
-
-		int[] vertexIndices = surface.getVertexIndices(this.polyhedron3D);
-		Point_3[] points = new Point_3[3];
-		for(int i=0; i < 3; i++)
-			points[i] = this.polyhedron3D.vertices.get(vertexIndices[i]).getPoint();
-		a = (double) points[0].distanceFrom(points[1]);
-		b = (double) points[1].distanceFrom(points[2]);
-		c = (double) points[0].distanceFrom(points[2]);
-		S = 0.5 * (a+b+c);
-		A = Math.sqrt(S*(S-a)*(S-b)*(S-c));
+		
+		double A = this.areaOfTriangle(surface);
 
 		return A*Math.pow(distance, 2);
 	}
